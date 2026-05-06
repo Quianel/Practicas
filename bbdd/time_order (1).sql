c739bf922eac506f823ff9947ee0279c96c85dff
Tu nombre <danielgarorte@gmail.com>
2026-05-06 17:42:26 +0200

    Subo la base de datos con el create database y use database

--- bbdd/time_order (1).sql
+++ bbdd/time_order (1).sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Base de datos: `time_order`
 --
 
+CREATE DATABASE `TIME_ORDER`;
+USE `TIME_ORDER`;
+
 -- --------------------------------------------------------
 
 --
