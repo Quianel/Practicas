b0f535faef05549a3647e8703dbdc736326811ae
nadirallub-j23 <nadir.allub@juanxxiii.net>
2026-05-12 18:46:50 +0200

    Inicio ventana reportes - Nadir

---
+++ src/vista/VentanaCentroReportes.java
@@ -0,0 +1,16 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class VentanaCentroReportes extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public VentanaCentroReportes() {
+		
+	}
+
+}
