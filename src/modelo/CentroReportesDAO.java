a626035275c6d502e03a90ca696317923ff50f8a
Tu nombre <danielgarorte@gmail.com>
2026-05-14 01:47:29 +0200

    Subo la tabla de centro de reportes con el combobox proyectos funcionando

--- src/modelo/CentroReportesDAO.java
+++ src/modelo/CentroReportesDAO.java
@@ -3,8 +3,11 @@ package modelo;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 import javax.swing.JComboBox;
+import javax.swing.table.DefaultTableModel;
 
 public class CentroReportesDAO {
 
@@ -47,4 +50,277 @@ public class CentroReportesDAO {
 		}
 	}
 
+	public void TablaProyectosActivos(DefaultTableModel modelotabla) {
+		Connection conexion;
+		Statement consulta;
+		ResultSet registro;
+		int Proyectos_Activos = 0;
+		try {
+			conexion = ConexionBD.getConexion();
+			consulta = conexion.createStatement();
+			registro = consulta.executeQuery("select count(id_proyecto) as 'Proyectos_Activos' "
+					+ "from proyecto, estado_proyecto "
+					+ "where proyecto.id_estado_proyecto = estado_proyecto.id_estado_proyecto "
+					+ "and estado_proyecto.nombre = 'activo';");
+			
+			if(registro.next()) {
+				Proyectos_Activos = registro.getInt("Proyectos_Activos");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		modelotabla.addRow(new Object[] {
+				"Proyectos activos", Proyectos_Activos
+		});		
+	}
+
+	public void TablaTrabajadoresActivos(DefaultTableModel modelotabla) {
+		Connection conexion;
+		Statement consulta;
+		ResultSet registro;
+		int Trabajadores_Activos = 0;
+		try {
+			conexion = ConexionBD.getConexion();
+			consulta = conexion.createStatement();
+			registro = consulta.executeQuery("select count(id_trabajador) as 'Trabajadores_Activos' "
+					+ "from trabajador "
+					+ "where activo = true");
+			
+			if(registro.next()) {
+				Trabajadores_Activos = registro.getInt("Trabajadores_Activos");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		modelotabla.addRow(new Object[] {
+				"Trabajadores activos", Trabajadores_Activos
+		});		
+	}
+
+	/**
+	 * Hace un select de la base de datos para tomar las fechas, entonces las guarda como LocalDate y mientras que la primera fecha 
+	 * no llegue a la segunda, irá preguntando si el día de la primera fecha es sábado o domingo, si no lo es, aumentará uno
+	 * los dias laborales e inicio pasará al día siguiente, si es sábado o domingo, simplemente inicio pasa al día siguiente.
+	 * 
+	 * Una vez que la fecha uno haya terminado de recorrer todos los días (incluído el último día, el de la segunda fecha), el número
+	 * de días laborales se multiplicará por 8 horas para, así, descubrir la cantidad de horas asignadas
+	 * @return el total de horas asignadas.
+	 */
+	public int obtenerHorasAsignadas() {
+		Connection conexion;
+		Statement consulta;
+		ResultSet registro;
+		int HorasAsignadas = 0;
+		int diasLaborables = 0;
+		try {
+			conexion = ConexionBD.getConexion();
+			consulta = conexion.createStatement();
+			registro = consulta.executeQuery("select fecha_asignacion as 'FechaAsignacion', fecha_fin_asignacion as 'FechaFin' "
+					+ "from asignacion_tarea");
+			while(registro.next()) {
+				LocalDate inicio = registro.getDate("FechaAsignacion").toLocalDate();
+				LocalDate fin = registro.getDate("FechaFin").toLocalDate();
+
+				while (!inicio.isAfter(fin)) {
+
+				    DayOfWeek dia = inicio.getDayOfWeek();
+
+				    if (dia != DayOfWeek.SATURDAY &&
+				        dia != DayOfWeek.SUNDAY) {
+
+				        diasLaborables++;
+				    }
+
+				    inicio = inicio.plusDays(1);
+				}
+			}
+			
+			HorasAsignadas = diasLaborables * 8;
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return HorasAsignadas;
+	}
+
+	public void TablaHorasAsignadas(DefaultTableModel modelotabla, int horasAsignadas) {
+		modelotabla.addRow(new Object[] {
+				"Horas asignadas", horasAsignadas + "h"
+		});	}
+
+	public int obtenerHorasTotales() {
+		Connection conexion;
+		Statement consulta;
+		ResultSet registro;
+		int horasTotales = 0;
+		try {
+			conexion = ConexionBD.getConexion();
+			consulta = conexion.createStatement();
+			registro = consulta.executeQuery("SELECT round(SUM(minutos_totales)/60,2) as 'horasTotales'"
+					+ "from registro_tiempo;");
+			
+			if(registro.next()) {
+				horasTotales = registro.getInt("horasTotales");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return horasTotales;
+	}
+
+	public void TablaHorasTotales(DefaultTableModel modelotabla, int horasTotales) {
+		modelotabla.addRow(new Object[] {
+				"Horas totales", horasTotales + "h"
+		});	
+	}
+
+	public void TablaCumplimiento(DefaultTableModel modelotabla, int horasTotales, int horasAsignadas) {
+		float resultado = (horasTotales * 100f)/horasAsignadas;
+		modelotabla.addRow(new Object[] {
+				"Cumplimiento", resultado + "%"
+		});	
+	}
+
+	public int obtenerHorasTotalesConFiltro(String proyectoSeleccionado) {
+		Connection conexion;
+		Statement consulta;
+		ResultSet registro = null;
+		int horasTotales = 0;
+		try {
+			conexion = ConexionBD.getConexion();
+			consulta = conexion.createStatement();
+			
+			if(proyectoSeleccionado.equals("(Todos)")) {
+				registro = consulta.executeQuery("SELECT round(SUM(minutos_totales)/60,2) as 'horasTotales'"
+						+ "from registro_tiempo;");
+			}else {
+				registro = consulta.executeQuery("SELECT round(SUM(registro_tiempo.minutos_totales)/60,2) as 'horasTotales' "
+						+ "from registro_tiempo, proyecto "
+						+ "where registro_tiempo.id_proyecto = proyecto.id_proyecto "
+						+ "and proyecto.nombre = '" + proyectoSeleccionado + "'");	
+			}
+			
+			if(registro.next()) {
+				horasTotales = registro.getInt("horasTotales");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return horasTotales;
+	}
+
+	public int obtenerHorasAsignadasConFiltro(String proyectoSeleccionado) {
+		Connection conexion;
+		Statement consulta;
+		ResultSet registro = null;
+		int HorasAsignadas = 0;
+		int diasLaborables = 0;
+		try {
+			conexion = ConexionBD.getConexion();
+			consulta = conexion.createStatement();
+			
+			if(proyectoSeleccionado.equals("(Todos)")) {
+				registro = consulta.executeQuery("select fecha_asignacion as 'FechaAsignacion', fecha_fin_asignacion as 'FechaFin' "
+						+ "from asignacion_tarea");
+			}else {
+				registro = consulta.executeQuery("select fecha_asignacion as 'FechaAsignacion', fecha_fin_asignacion as 'FechaFin' "
+						+ "from asignacion_tarea, tarea_proyecto, proyecto "
+						+ "where asignacion_tarea.id_tarea_proyecto = tarea_proyecto.id_tarea_proyecto "
+						+ "and tarea_proyecto.id_proyecto = proyecto.id_proyecto "
+						+ "and proyecto.nombre = '" + proyectoSeleccionado + "'");
+			}
+			while(registro.next()) {
+				LocalDate inicio = registro.getDate("FechaAsignacion").toLocalDate();
+				LocalDate fin = registro.getDate("FechaFin").toLocalDate();
+
+				while (!inicio.isAfter(fin)) {
+
+				    DayOfWeek dia = inicio.getDayOfWeek();
+
+				    if (dia != DayOfWeek.SATURDAY &&
+				        dia != DayOfWeek.SUNDAY) {
+
+				        diasLaborables++;
+				    }
+
+				    inicio = inicio.plusDays(1);
+				}
+			}
+			
+			HorasAsignadas = diasLaborables * 8;
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return HorasAsignadas;
+	}
+
+	public void TablaProyectosActivosConFiltro(DefaultTableModel modelotabla, String proyectoSeleccionado) {
+		Connection conexion;
+		Statement consulta;
+		ResultSet registro = null;
+		int Proyectos_Activos = 0;
+		try {
+			conexion = ConexionBD.getConexion();
+			consulta = conexion.createStatement();
+			if(proyectoSeleccionado.equals("(Todos)")) {
+			registro = consulta.executeQuery("select count(id_proyecto) as 'Proyectos_Activos' "
+					+ "from proyecto, estado_proyecto "
+					+ "where proyecto.id_estado_proyecto = estado_proyecto.id_estado_proyecto "
+					+ "and estado_proyecto.nombre = 'activo';");
+			}else {
+				registro = consulta.executeQuery("select count(id_proyecto) as 'Proyectos_Activos' "
+						+ "from proyecto, estado_proyecto "
+						+ "where proyecto.id_estado_proyecto = estado_proyecto.id_estado_proyecto "
+						+ "and estado_proyecto.nombre = 'activo'"
+						+ "and proyecto.nombre = '" + proyectoSeleccionado + "'");		
+			}
+			
+			if(registro.next()) {
+				Proyectos_Activos = registro.getInt("Proyectos_Activos");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		modelotabla.addRow(new Object[] {
+				"Proyectos activos", Proyectos_Activos
+		});	
+	}
+
+	public void TablaTrabajadoresActivosConFiltro(DefaultTableModel modelotabla, String proyectoSeleccionado) {
+		Connection conexion;
+		Statement consulta;
+		ResultSet registro = null;
+		int Trabajadores_Activos = 0;
+		try {
+			conexion = ConexionBD.getConexion();
+			consulta = conexion.createStatement();
+			if(proyectoSeleccionado.equals("(Todos)")) {
+				registro = consulta.executeQuery("select count(id_trabajador) as 'Trabajadores_Activos' "
+						+ "from trabajador, rol_sistema "
+						+ "where activo = true "
+						+ "and trabajador.id_rol = rol_sistema.id_rol "
+						+ "and rol_sistema.nombre = 'Trabajador'");
+			}else {
+			registro = consulta.executeQuery("select count(trabajador.id_trabajador) as 'Trabajadores_Activos' "
+					+ "from trabajador, asignacion_proyecto, proyecto "
+					+ "where activo = true "
+					+ "and trabajador.id_trabajador = asignacion_proyecto.id_trabajador "
+					+ "and asignacion_proyecto.id_proyecto = proyecto.id_proyecto "
+					+ "and proyecto.nombre = '" + proyectoSeleccionado + "'");
+			}
+			if(registro.next()) {
+				Trabajadores_Activos = registro.getInt("Trabajadores_Activos");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		modelotabla.addRow(new Object[] {
+				"Trabajadores activos", Trabajadores_Activos
+		});	
+	}
 }
--- src/vista/VentanaCentroReportes.java
+++ src/vista/VentanaCentroReportes.java
@@ -8,13 +8,24 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import com.toedter.calendar.JDateChooser;
 import java.awt.Color;
+import java.awt.DefaultFocusTraversalPolicy;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaCentroReportes extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private JComboBox cmbProyecto;
 	private JComboBox cmbTipoProyecto;
+	private JTable tblHoras;
+	private JScrollPane scrollPane;
 
+	
+	DefaultTableModel modelotabla = new DefaultTableModel();
 	/**
 	 * Create the panel.
 	 */
@@ -22,6 +33,21 @@ public class VentanaCentroReportes extends JPanel {
 		setLayout(null);
 		
 		cmbProyecto = new JComboBox();
+		cmbProyecto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String proyectoSeleccionado = (String) cmbProyecto.getSelectedItem();
+				CentroReportesDAO centRepBD = new CentroReportesDAO();
+				modelotabla.setRowCount(0);
+				
+				int horasTotales = centRepBD.obtenerHorasTotalesConFiltro(proyectoSeleccionado);
+				centRepBD.TablaHorasTotales(modelotabla, horasTotales);
+				int horasAsignadas = centRepBD.obtenerHorasAsignadasConFiltro(proyectoSeleccionado);
+				centRepBD.TablaHorasAsignadas(modelotabla, horasAsignadas);
+				centRepBD.TablaCumplimiento(modelotabla, horasTotales, horasAsignadas);
+				centRepBD.TablaProyectosActivosConFiltro(modelotabla, proyectoSeleccionado);
+				centRepBD.TablaTrabajadoresActivosConFiltro(modelotabla, proyectoSeleccionado);
+			}
+		});
 		cmbProyecto.setBounds(100, 10, 225, 20);
 		add(cmbProyecto);
 		
@@ -57,12 +83,35 @@ public class VentanaCentroReportes extends JPanel {
 		lblFecha2.setBounds(210, 70, 50, 20);
 		add(lblFecha2);
 		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 100, 340, 190);
+		add(scrollPane);
+		
+		tblHoras = new JTable();
+		scrollPane.setViewportView(tblHoras);
+		
+		modelotabla.setColumnIdentifiers(new Object[] {"Resumen","Datos"});
+		tblHoras.setModel(modelotabla);
+		modelotabla.setRowCount(0);
+		
 		CargaDeComboBoxProyectos();
+		//cargaTabla();
 	}
 	
 	private void CargaDeComboBoxProyectos() {
 		CentroReportesDAO centRepBD = new CentroReportesDAO();
 		centRepBD.Combobox1(cmbProyecto);
 		centRepBD.Combobox2(cmbTipoProyecto);
+	}
+	
+	private void cargaTabla() {
+		CentroReportesDAO centRepBD = new CentroReportesDAO();
+		int horasTotales = centRepBD.obtenerHorasTotales();
+		centRepBD.TablaHorasTotales(modelotabla, horasTotales);
+		int horasAsignadas = centRepBD.obtenerHorasAsignadas();
+		centRepBD.TablaHorasAsignadas(modelotabla, horasAsignadas);
+		centRepBD.TablaCumplimiento(modelotabla, horasTotales, horasAsignadas);
+		centRepBD.TablaProyectosActivos(modelotabla);
+		centRepBD.TablaTrabajadoresActivos(modelotabla);
 	}
 }
